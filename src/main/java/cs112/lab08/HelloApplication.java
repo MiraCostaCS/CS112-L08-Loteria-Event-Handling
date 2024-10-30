0c542135ff13941ffa7a37699d973cf89997e9cc
Nery Chapetón-Lamas <nchapetonLamas@miracosta.edu>
2024-10-30 15:07:40 -0700

    cleanup like removing leftover Main.java file

--- src/main/java/cs112/lab08/HelloApplication.java
+++ src/main/java/cs112/lab08/HelloApplication.java
@@ -22,11 +22,7 @@ public class HelloApplication extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
+        //removed FXML code, fill this in with components, scene, stage, etc.
     }
 
     public static void main(String[] args) {
